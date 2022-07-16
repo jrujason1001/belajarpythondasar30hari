cb5ddcc608f99c95b6f89a85c57953f2c411c0bb
jrujason1001 <79412658+jrujason1001@users.noreply.github.com>
2022-07-16 21:50:54 +0700

    Menggantikan file gitignore menjadi intro.py

---
+++ hari1/intro.py
@@ -0,0 +1,3 @@
+print("Hello world") //Ini cara memanggil dalam python
+Variabel = 100 //Ini bisa diisi dengan string, integer, boolean, dan tipe data lainnya
+ask = input("Halo nama anda siapa?") //Untuk menanyakan user 
